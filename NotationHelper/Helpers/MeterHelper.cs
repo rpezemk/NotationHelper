64ad677f5eeb5c5e8d993102afed9e434f6dc711
przemyslaw zaremba <rpezemk@gmail.com>
2024-09-08 11:18:19 +0200

    duration to len @ MeterHelper

---
+++ NotationHelper/Helpers/MeterHelper.cs
@@ -0,0 +1,13 @@
+﻿using NotationHelper.DataModel.Elementary;
+
+namespace NotationHelper.Helpers
+{
+    public static class MeterHelper
+    {
+        public static double DurationToLength(this Duration duration)
+        {
+            var absLen = duration.GetInLength();
+            return absLen;
+        }
+    }
+}
