b8bd65e85d684840d21705a6c3d1319020cb648f
przemyslaw zaremba <rpezemk@gmail.com>
2024-09-27 22:02:01 +0200

    multilayer instrument (dynamics)

--- AudioTool/Instruments/LayerSampleInstrument.cs
+++ AudioTool/Instruments/LayerSampleInstrument.cs
@@ -3,17 +3,43 @@ using AudioTool.Instruments.InstrumentBase;
 
 namespace AudioTool.Instruments
 {
+    public class Layer
+    {
+        public Layer(int no, string dyn, string instrName, string basePath, string extension) 
+        {
+            No = no;
+            Dyn = dyn;
+            InstrName = instrName;
+            BasePath = basePath;
+            Extension = extension;
+        }
+        public int No;
+        public string Dyn;
+        public string InstrName;
+        public string BasePath;
+        public string Extension;
+        public string FilePath => $"{BasePath}{Dyn}-{InstrName}.{Extension}";
+    }
+    public static class InstrumentFileHelper
+    {
+        public static List<Layer> ViolaLayers = 
+            [
+                new (0, "P", "Viole", @"C:/Users/slonj/Desktop/Samples/Bowed_", "wav"),
+                new (1, "MP", "Viole", @"C:/Users/slonj/Desktop/Samples/Bowed_", "wav"),
+                new (2, "MF", "Viole", @"C:/Users/slonj/Desktop/Samples/Bowed_", "wav"),
+                new (3, "F", "Viole", @"C:/Users/slonj/Desktop/Samples/Bowed_", "wav"),
+                new (4, "FF", "Viole", @"C:/Users/slonj/Desktop/Samples/Bowed_", "wav"),
+            ];
+    }
+
+
     public class LayerSampleInstrument : AScriptedInstrument<LayerSampleEvent>
     {
-        public string FilePathPiano = @"C:/Users/slonj/Desktop/Samples/Bowed_P-Viole.wav";
-        public string FilePathForte = @"C:/Users/slonj/Desktop/Samples/Bowed_F-Viole.wav";
-        
-        public LayerSampleInstrument(string name) : base(name)
+        public List<Layer> Layers { get; set; } = new List<Layer>();
+        public LayerSampleInstrument(string name, List<Layer> layers) : base(name) 
         {
+            Layers = layers;
         }
-
-        
-
         public override string GetInstrumentScript(int instrNo, string instrName)
         {
             var part =
@@ -23,71 +49,30 @@ namespace AudioTool.Instruments
         ;##############################################################
         instr {instrNo}
             kenv linseg 0, p3, 1
-            aLeft_Piano, aRight_Piano diskin2 ""{FilePathPiano}"", 1, p4
-            aOutL_p atone aLeft_Piano,  230
-            aOutR_p atone aRight_Piano, 230
-            aLeft_Forte, aRight_Forte diskin2 ""{FilePathForte}"", 1, p4
-            aOutL_f atone aLeft_Forte,  230
-            aOutR_f atone aRight_Forte, 230
-            iDynCount = 5                ; Number of dynamic files
-            iCheckedDynNo = 2            ; Example checked dynamic number
-            iCurrValue = 3               ; Example current value
+            {InstrumentFileHelper.ViolaLayers.Select(l => l.ToFileLoad()).JoinToBlock(18)}
 
-            kmod chnget ""{instrNo.ToModulatorStr()}""
-            ;event_i ""i"" ,{instrNo + 20}, 0, 0, 2, kmod, 0
-            ;event_i ""i"" ,{instrNo + 20}, 0, 0, 2, kmod, 1 
+            kMod chnget ""{instrNo.ToModulatorStr()}""
+            
+            {InstrumentFileHelper.ViolaLayers.Select(l => l.ToCalculatedDynamics(InstrumentFileHelper.ViolaLayers.Count())).JoinToBlock(18)}
 
-            aOutL = 0.2*aOutL_p * (1-kmod) + 0.2*aOutL_f * kmod
-            aOutR = 0.2*aOutR_p * (1-kmod) + 0.2*aOutR_f * kmod
-
+            aOutL = 0.1 * ({InstrumentFileHelper.ViolaLayers.ToOutput("Left")})
+            aOutR = 0.1 * ({InstrumentFileHelper.ViolaLayers.ToOutput("Right")})
+            
             outs aOutL, aOutR
         endin
 
-
-        ;;;;;;;;;;;;;;;;;;;;DYNAMICS_MODULATOR {instrNo} => {instrNo.ToEnvelopeNo()} {instrName}       
-        ;;;;;;;;;;;;;;;;;;;instr {instrNo.ToEnvelopeNo()}
-        ;;;;;;;;;;;;;;;;;;;        kfreq init 0.7                     ; Set LFO frequency to 5 Hz
-        ;;;;;;;;;;;;;;;;;;;        ; Generate the LFO signal
-        ;;;;;;;;;;;;;;;;;;;        kLFO oscili 1, kfreq, 1          ; LFO signal with amplitude 1
-        ;;;;;;;;;;;;;;;;;;;        kmodulatedValue = (kLFO + 1) * 0.5 ; Normalize to 0-1 range
-        ;;;;;;;;;;;;;;;;;;;        chnset kmodulatedValue, ""{instrNo.ToModulatorStr()}""
-        ;;;;;;;;;;;;;;;;;;;endin
-
         ;DYNAMICS_MODULATOR
-        instr {instrNo.ToEnvelopeNo()}
+        instr {instrNo.ToEnvelopeInstrument()}
                 prints ""PARAMETER: p4 = %f \n"", p4   ; 
                 ;prints ""p6 = %f \n"", p6   ; 
-                itest chnget ""{instrNo.ToModulatorStr()}""
-                kenv linseg itest, p3, p4
+                iCurrEnv chnget ""{instrNo.ToModulatorStr()}""
+                kenv linseg iCurrEnv, p3, p4
                 chnset kenv, ""{instrNo.ToModulatorStr()}""
         endin
-        
-        instr {instrNo + 20} 
-            iRes init 0
-            iDynCount = p4             ; N-files
-            iMaxDyn = iDynCount - 1         
-            iCheckedDynNo = p6 * iMaxDyn ; [0...N-1]
-            iCurrValue = p5 * iMaxDyn   ; [0..N-1]       ; ""kenv""
-            iDiff = iCurrValue - iCheckedDynNo
-
-            if iDiff > 1 || iDiff < -1 then 
-                iRes = 0
-            endif
-            if iDiff > 0 then
-                iRes = iDiff 
-            endif
-            if iDiff < 0 then
-                iRes = -iDiff 
-            endif
-    
-            chnset iRes, ""myTestChan""   ;     
-        endin
-        
-
-
 ";
             return part;
         }
+
         public override List<string> GetEventsScript(int instrNo, string instrName)
         {
             return [];//$"i{instrNo.ToEnvelopeNo()} 0 10 1 ; i99 start at 0, 10 hours long, amp .0001"
@@ -100,17 +85,55 @@ namespace AudioTool.Instruments
         public static string ToLeftAudioOut(this int instrNo) => $"audio_out_left_{instrNo}";
         public static string ToRightAudioOut(this int instrNo) => $"audio_out_right_{instrNo}";
         public static string ToModulatorStr(this int instrNo) => $"MODULATOR_{instrNo}";
-        public static int ToEnvelopeNo(this int instrNo) => instrNo + 10;
-
-        public static string ToFileLoad(string filePath, int dynamics)
+        public static int ToEnvelopeInstrument(this int instrNo) => instrNo + 10;
+        public static string JoinToBlock(this IEnumerable<string> values, int indent) => "\n" + string.Join("\n", values.Select(v => "".PadLeft(indent) + v));
+        public static string ToFileLoad(this Layer layer)
         {
-            var str = @$"            
-            aLeft_{dynamics}, aRight_{dynamics} diskin2 ""{filePath}"", 1, p4
-            aOutL_{dynamics} atone aLeft_{dynamics},  230
-            aOutR_{dynamics} atone aRight_{dynamics}, 230";
+            var str = @$"
+                iSampleLen = p4
+                ;;;;; TODO smoothly loop
+                kTimeEnv_{layer.Dyn} linseg 0, iSampleLen, iSampleLen
+
+                aLeft_{layer.Dyn}_wav1, aRight_{layer.Dyn}_wav1 diskin2 ""{layer.FilePath}"", 1, iSampleLen
+
+                aLeft_{layer.Dyn} = aLeft_{layer.Dyn}_wav1
+                aRight_{layer.Dyn} = aRight_{layer.Dyn}_wav1
+                ";
             return str;
         }
 
+        public static string ToKMod(this int instrNo) => $"kMod_{instrNo}";
+
+        /// <summary>
+        /// kDiff_{layer.No}
+        /// </summary>
+        /// <param name="layer"></param>
+        /// <param name="instrNo"></param>
+        /// <param name="nLayers"></param>
+        /// <returns></returns>
+        public static string ToCalculatedDynamics(this Layer layer, int nLayers)
+        {
+            var res = @$"            
+            kDiff_{layer.No} = kMod * {nLayers - 1} - {layer.No}
+            kRes_{layer.No} init 0
+            if kDiff_{layer.No} > 1 || kDiff_{layer.No} < -1 then 
+                kRes_{layer.No} = 0
+            elseif kDiff_{layer.No} >= 0 then
+                kRes_{layer.No} =   (1 - kDiff_{layer.No})
+            elseif kDiff_{layer.No} < 0 then
+                kRes_{layer.No} = - (1 - kDiff_{layer.No})
+            endif
+            ";
+            return res;
+        }
+
+        public static string ToOutput(this List<Layer> layers, string side)
+        {
+            //0.2*aLeft_P * (1-kMod) + 0.2*aLeft_F * kMod
+
+            var res = string.Join(" + ", layers.Select(l => $"a{side}_{l.Dyn} * kRes_{l.No}"));
+            return res;
+        }
 
     }
 }
--- AudioTool/Program.cs
+++ AudioTool/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         var noise = new NoiseInstrument("NOISE");
         var flatViola = new FlatSampleInstrument("VIOLA_01");
-        var layeredViola = new LayerSampleInstrument("VIOLA_01");
+        var layeredViola = new LayerSampleInstrument("VIOLA_01", InstrumentFileHelper.ViolaLayers);
         CsEngine csEngine = new CsEngine([noise, flatViola, layeredViola]);
         csEngine.RunAsync();
         var pitches = new List<int>() { 0, 7, 12, 16 };
@@ -25,20 +25,22 @@ class Program
             }
         });
 
-        Task.Run(() =>
-        {
-            while (true)
-            {
-                foreach (var p in pitches)
-                {
-                    //csEngine.Play(flatViola.EmitFromInstr(new FlatSampleEvent(0, 4, p * 4)));
-                    csEngine.Play(layeredViola.EmitFromInstr(new LayerSampleEvent(0, 1, 1, 0, 0)).AsModulator());
-                    Thread.Sleep(1000);
-                    csEngine.Play(layeredViola.EmitFromInstr(new LayerSampleEvent(0, 1, 0, 0, 0)).AsModulator());
-                    Thread.Sleep(1000);
-                }
-            }
-        });
+        //var dynamicsDelay = 10;//s
+        //var dynamics = 1;
+        //Task.Run(() =>
+        //{
+        //    while (true)
+        //    {
+        //        foreach (var p in pitches)
+        //        {
+        //            //csEngine.Play(flatViola.EmitFromInstr(new FlatSampleEvent(0, 4, p * 4)));
+        //            csEngine.Play(layeredViola.EmitFromInstr(new LayerSampleEvent(0, dynamicsDelay, dynamics, 0, 0)).AsModulator());
+        //            Thread.Sleep(dynamicsDelay * 1000);
+        //            csEngine.Play(layeredViola.EmitFromInstr(new LayerSampleEvent(0, dynamicsDelay, dynamics, 0, 0)).AsModulator());
+        //            Thread.Sleep(dynamicsDelay * 1000);
+        //        }
+        //    }
+        //});
 
 
         Thread.Sleep(3600 * 1000);
